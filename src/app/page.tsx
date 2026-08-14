f078398b5dfc7bdd7f83cce439701dee704dc082
Park-junSungT <202310206@icr.icems.kr>
2026-08-14 22:28:33 +0900

    modified app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import { ExperienceRoot } from "@/components/experience/ExperienceRoot";
 import { Analytics } from "@vercel/analytics/next"
 
 export default function Page() {
-  return <ExperienceRoot />;
-  <Analytics />
+  return (
+    <>
+      <ExperienceRoot />
+      <Analytics />
+    </>
+  )
 }
