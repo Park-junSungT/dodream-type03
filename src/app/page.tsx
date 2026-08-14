31e52dc8719f56f865c76b160cdd6a062e522f49
Park-junSungT <202310206@icr.icems.kr>
2026-08-14 20:16:20 +0900

    Add vercel Analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { ExperienceRoot } from "@/components/experience/ExperienceRoot";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Page() {
   return <ExperienceRoot />;
+  <Analytics />
 }
